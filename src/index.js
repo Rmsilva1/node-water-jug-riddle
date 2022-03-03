3a854010071ec97f2fbf730859439aea05a7f960
Rafael Mateus <rafael.mateus.silva@hotmail.com>
2022-03-03 00:32:13 -0300

    remove unused dependencies

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-
 const app = express();
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
-
 
 app.post('/', (req, res) => {
     const { bucketXCapacity, bucketYCapacity, measureAmount } = req.body;
